13a6ec95a5dda1b48436f6eb3d859cd6578ebdbb
Exiam <exiam@pm.me>
2020-03-15 01:27:31 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,23 @@
+## Template for basic TypeScript-HTML projects
+
+This template will provide a TypeScript installation with linter (eslint), formatter (prettier) and webpack for HTML based projects.
+
+### Run dev server
+
+```
+npm run serve
+```
+
+### Build
+
+```
+npm run build
+```
+
+### Dependencies
+
+Dev dependencies:
+
+```
+npm install --save-dev typescript eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier webpack webpack-cli webpack-dev-server ts-loader html-webpack-plugin
+```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+};
