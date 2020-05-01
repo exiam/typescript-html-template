47e405b122ea9250a159f4c9485fffa42518650c
exiam <exiam@pm.me>
2020-05-01 16:26:47 +0200

    Add scripts and update config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: './src/index.ts',
   module: {
     rules: [
